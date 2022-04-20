e48b787c745962d90f59ce0cbee9fd4722e0fb8e
sajjad960 <sajjadhossainsb01@gmail.com>
2022-04-20 20:53:36 +0600

    some essential command

--- BashScript/bestdayever.sh
+++ BashScript/bestdayever.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-name="Ben"
+# Declaring variable
+# name="Ben"
+
+# Let's ask for name and print it
+# echo "What's you name"
+
+# read name
+
+# put name when run this script
+name=$1 #Positional parameters
+
 
 echo "Good Morning $name"
 sleep 1s
 echo "You're looking good $name"
 sleep 1s
-echo "You have the best bread I'v ever seen $name"
+echo "You have the best $2 I'v ever seen $name"
