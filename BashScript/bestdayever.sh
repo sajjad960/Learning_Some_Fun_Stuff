809d2d032552899080bf99c6dbb3fec0359ce5ea
sajjad960 <sajjadhossainsb01@gmail.com>
2022-04-20 21:07:54 +0600

    practised some variable

--- BashScript/bestdayever.sh
+++ BashScript/bestdayever.sh
@@ -11,9 +11,20 @@
 # put name when run this script
 name=$1 #Positional parameters
 
+# Some more variable
+user=$(whoami)
+whereami=$(pwd)
+date=$(date)
+wheather=$(curl wttr.in)
+
 
 echo "Good Morning $name"
 sleep 1s
 echo "You're looking good $name"
 sleep 1s
 echo "You have the best $2 I'v ever seen $name"
+sleep 2
+
+echo "You are currently logged in as $user and you are in the directory $whereami. ALso tody is: $date 😅😅"
+
+echo "$wheather wheather prited"
